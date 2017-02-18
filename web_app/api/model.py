83ba86ba8044c9445b1ef5360e7a0d498087ae1d
Zachary Clark <zmclark@umich.edu>
2017-02-18 16:17:21 -0500

    Implements version 1 of model.py

--- web_app/api/model.py
+++ web_app/api/model.py
@@ -8,4 +8,20 @@ model = Blueprint('model', __name__, template_folder='templates')
 #-------------#
 @model.route('/api/v1/model', methods=['GET', 'POST', 'DELETE'])
 def model_route():
+	db = connect_to_database()
+	json_data = request.get_json()
+
+	if data_missing_keys(json_data, ['']):
+		return 422
+
+	cur = db.cursor()
+	cur.execute('SELECT ')
+
     return render_template('index.html')
+
+def data_missing_keys(json_data, required_keys):
+	missing_keys = False
+	for key in required_keys():
+		if key not in json_data:
+			missing_keys = True
+	return missing_keys
