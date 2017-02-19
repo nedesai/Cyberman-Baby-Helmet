6ff9f90d376133adb46d2862117f03c50f07736b
Kuong Tran <kuong@umich.edu>
2017-02-19 17:27:25 -0500

    update http requests again

--- web_app/static/js/directives/models.js
+++ web_app/static/js/directives/models.js
@@ -6,7 +6,7 @@ app.directive('models', ['$http', function($http){
 			patientid: "="
 		},
 		controller: function($scope) {
-			$scope.models = $http.get("api/v1/model/?username=" + $scope.username + "&patientid=" + $scope.patientid);
+			$scope.models = $http.get("api/v1/model?username=" + $scope.username + "&patientid=" + $scope.patientid);
 		},
 		templateUrl: 'static/js/directives/models.html',
 		link: function(scope, element, attrs) {
--- web_app/static/js/directives/patients.js
+++ web_app/static/js/directives/patients.js
@@ -5,7 +5,7 @@ app.directive('patients', ['$http', function($http){
 			username: "="
 		},
 		controller: function($scope) {
-			$scope.patients = $http.get("api/v1/patient/?username=" + $scope.username);
+			$scope.patients = $http.get("api/v1/patient?username=" + $scope.username);
 		},
 		templateUrl: 'static/js/directives/patients.html',
 		link: function(scope, element, attrs) {
