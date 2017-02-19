3cc3ca89a9c6c5feb702db78f44d5a310e90b2f3
Kuong Tran <kuong@umich.edu>
2017-02-19 16:10:00 -0500

    link locations

--- web_app/static/js/directives/models.js
+++ web_app/static/js/directives/models.js
@@ -5,7 +5,9 @@ app.directive('models', ['http', function($http){
 			username: "=",
 			patientid: "="
 		},
-		//controller,
+		controller: function($scope) {
+			$scope.models = $http.get("api/models/?username=" + $scope.username + "&patientid=" + $scope.patientid);
+		},
 		templateUrl: 'static/js/directives/models.html',
 		link: function(scope, elem, attrs) {
 			scope.func = function(flag) {
