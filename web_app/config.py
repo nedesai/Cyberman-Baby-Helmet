1e4df02d368665661ee8e33d2f789e98a87b6c25
Zachary Clark <zmclark@umich.edu>
2017-02-19 18:05:34 -0500

    Removes a comment

--- web_app/config.py
+++ web_app/config.py
@@ -1,6 +1,3 @@
-# Do NOT commit this file to github
-# Make a seperate one for your deployed environment
-# Do not change the host, it is referring to database host not website host
 env = dict(
 	host = '0.0.0.0',
 	port = 3000,
