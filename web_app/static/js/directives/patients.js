f0c516db3438d09eafb4bef3c0d376ac3e7043b6
Kuong Tran <kuong@umich.edu>
2017-02-19 19:20:23 -0500

    modified patients

--- web_app/static/js/directives/patients.js
+++ web_app/static/js/directives/patients.js
@@ -6,7 +6,7 @@ app.directive('patients', ['$http', function($http){
 		},
 		controller: function($scope) {
 			var results = $http.get("api/v1/patient?username=" + $scope.username);
-			$scope.patients = results.patients
+			$scope.patients = results.patients;
 		},
 		templateUrl: 'static/js/directives/patients.html',
 		link: function(scope, element, attrs) {
