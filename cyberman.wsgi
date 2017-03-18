15f455c945aa11019ba9ab00c7d5fccc04895aa3
vagrant <vagrant@vagrant.vm>
2017-03-18 17:17:42 +0000

    removed uneccessary files

--- cyberman.wsgi
+++
@@ -1,3 +0,0 @@
-import sys
-sys.path.insert(0, '/srv/Cyberman-Baby-Helmet/web_app')
-from web_app import app as application
--- static/js/controllers/CybermanController.js
+++
@@ -1,9 +0,0 @@
-app.controller('CybermanController', ['$scope', function($scope) {
-	
-	$scope.username = "headmodel22";
-	
-	$scope.obj = {
-		patientid: -1, 
-		viewmodel: false
-	};
-}]);
