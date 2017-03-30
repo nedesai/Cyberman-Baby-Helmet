3bb864e5b615f0054aee1d1783171f3734111d9d
nedesai <nedesai@umich.edu>
2017-03-29 22:46:54 -0400

    update processobj

--- web_app/api/model.py
+++ web_app/api/model.py
@@ -69,8 +69,8 @@ def processobj(file, filename):
     #convert obj to fbx and save it at fbxpath
     obj2fbx(objpath, fbxpath)
     #upload both obj and fbx
-    obj_url = uploads3(open(objpath,'r'),filename + '.obj')
-    fbx_url = uploads3(open(fbxpath,'r'),filename + '.fbx')
+    obj_url = uploads3(objpath,filename + '.obj')
+    fbx_url = uploads3(fbxpath,filename + '.fbx')
     os.remove(objpath)
     os.remove(fbxpath)
     return obj_url, fbx_url
