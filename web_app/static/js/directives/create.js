392440f01121a21e7e41c1755fd90bd54f831e8e
Kuong Tran <kuong@umich.edu>
2017-04-07 12:13:03 -0400

    create user directive

---
+++ web_app/static/js/directives/create.js
@@ -0,0 +1,35 @@
+app.directive('create', ['$http', 'SharedService', function($http, SharedService, location){
+	return {
+		restrict: 'E',
+		scope: {
+		},
+		templateUrl: 'static/js/directives/create.html',
+		link: function(scope, element, attrs) {
+
+			scope.directive_info = SharedService.sharedInfo;
+
+			scope.errors = [];
+
+			scope.create = function(){
+				
+				var dataobj = {
+					username: String(scope.create_username),
+					firstname: String(scope.create_firstname),
+					lastname: String(scope.create_lastname),
+					email: String(scope.create_email),
+					password1: String(scope.create_password1),
+					password2: String(scope.create_password2)
+				};
+				$http.post('api/v1/register', dataobj).then(
+					function(success){
+						scope.directive_info.log_in = true;
+					},
+					function(error){
+						scope.errors = [];
+						scope.errors.push(error.data.error);
+					}
+				);
+			}
+		}
+	}
+}]);
