8abd5c4e725b8231316cc87b7c03759ce0d65621
Kuong Tran <kuong@umich.edu>
2017-04-07 12:15:34 -0400

    fixed login api

--- web_app/api/login.py
+++ web_app/api/login.py
@@ -1,107 +1,59 @@
 from flask import *
 from flask import session
 from extensions import connect_to_database
-from datetime import datetime
 from .api_utilities import data_missing_keys, check_user_permissions, NO_ERRORS
-import os
+import MySQLdb
+import MySQLdb.cursors
 import hashlib
 import uuid
-import re
+
 
 login = Blueprint('login', __name__, template_folder='templates')
 
 @login.route('/api/v1/login', methods=['GET', 'POST'])
+
 def login_route():
-	print ("debug")
-	db = connect_to_database()
-	cur = db.cursor()
-	name = False
-	if 'username' in session:
-		user = session['username']
-		cur.execute('''Select firstname, lastname from User where username = ''' + "'" + user + "'")
-		name = cur.fetchall()
+  if request.method == 'GET':
+    if 'username' in session:
+      return jsonify(username=session['username'])
+    else:
+      return jsonify(username='')
+  if request.method == 'POST':
+    session.clear()
+    if 'username' in session:
+      redirect(url_for('main.main_route'))
+    else:
+      posteddata = request.get_json()
+      if 'username' not in posteddata or 'password' not in posteddata:
+        return jsonify(error="You did not provide the necessary fields"), 422
+      db = connect_to_database()
+      cur = db.cursor()
+      cur.execute("SELECT firstname, lastname, password FROM User WHERE username='" + posteddata['username'] + "'")
+      #Check for valid username
+      if cur.rowcount != 0:
+        results = cur.fetchall()
+        session['username'] = posteddata['username']
+        session['logged_in'] = True
+        results = results[0]
+        session['firstname'] = results['firstname']
+        session['lastname'] = results['lastname']
+        jsonobj = {'username': posteddata['username']}
 
-	if request.method == 'GET':
-		name = False
-		if 'username' in session:
-			user = session['username']
-			cur.execute('''Select firstname, lastname from User where username = ''' + "'" + user + "'")
-			name = cur.fetchall()
-		option = [
-			"user_not_found",
-			"pass_not_found",
-			name
-		]
-		
-		options = { "error" : option }
-		return jsonify(error=options)
-	if request.method == 'POST':
-		print ("hello")
-		json_data = request.get_json()
-		found_user = True
-		found_pass = True
-		user_input = json_data['username']
-		user_input = user_input.lower()
-		pass_input = json_data['password']
-		
-		print (pass_input + " " + user_input)
+        #Check for correct password
+        password_table = results['password']
+        
+        sep = password_table.find("$")
+        sep2 = password_table.rfind("$")
 
-		cur = db.cursor()
-		name = False
-		if 'username' in session:
-			user = session['username']
-			cur.execute('''Select firstname, lastname from User where username = ''' + "'" + user + "'")
-			name = cur.fetchall()
-		cur.execute('''Select username, password FROM User where username = ''' + "'" + user_input + "'")
-		msgs = cur.fetchall()
-
-		if not msgs:
-			print ("yes2332131")
-			error_msg = {"error_msg" : "please try again not found"}
-			print (error_msg)
-			return jsonify(error=error_msg), 404
-
-		split_pass = msgs[0]['password'].split('$', 2)
-		
-		print (split_pass)
-
-		#algorithm = 'sha512'
-		#salt = split_pass[1]
-		#m = hashlib.new(algorithm)
-		#m.update(salt + pass_input)
-		#password_hash = m.hexdigest()
-
-		if not msgs:
-			print ("no msgs return bad")
-			found_user = False
-		else:
-			#split_pass = msgs[0][1].split('$', 2)
-			print (split_pass[0] + " " + pass_input)
-			if split_pass[0] != pass_input:
-				found_pass = False
-		if found_user == False or found_pass == False:
-			option = [
-				"user_not_found " + str(found_user),
-				"pass_not_found " + str(found_pass),
-				name
-			]
-			
-			options = { "error" : option }
-			return jsonify(error=options)	
-		else:
-			name = False
-			session['username'] = user_input
-			if 'username' in session:
-				user = session['username']
-				cur.execute('''Select firstname, lastname from User where username = ''' + "'" + user + "'")
-				name = cur.fetchall()
-			#render page that the user came from
-			
-			option = [
-				"user_not_found",
-				"pass_not_found",
-				name
-			]
-			options = { "error" : option }
-			return jsonify(error=options)
-
+        hash_alg = password_table[0:sep]
+        salt = password_table[sep + 1: sep2]
+        m = hashlib.new(hash_alg)
+        m.update(salt + posteddata['password'])
+        password_hash = m.hexdigest()
+        password_check = hash_alg + "$" + salt + "$" + password_hash
+        if password_table != password_check:
+          session.clear()
+          return jsonify(error="Password is incorrect for the specified username"), 422
+        return jsonify(jsonobj), 200
+      else:
+        return jsonify(error="Username does not exist"), 404
