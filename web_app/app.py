b54d669fbd98de1455337db2f63ec1785131e32d
vagrant <vagrant@vagrant.vm>
2017-04-20 23:45:04 +0000

    photo email service

--- web_app/app.py
+++ web_app/app.py
@@ -23,8 +23,8 @@ app.secret_key = '4fd75asf_a8d4f_sad84f84'
 # Listen on external IPs
 if __name__ == '__main__':
     # Starts the photo email service as a separate thread
-    #pes = photo_email_service.PhotoEmailService()
-    #pes.start_listening()
+    pes = photo_email_service.PhotoEmailService()
+    pes.start_listening()
 
     # Listens on external IPs
     app.run(host=config.env['host'], port=config.env['port'], debug=True)
